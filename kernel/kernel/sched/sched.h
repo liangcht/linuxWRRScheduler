420b48986dab4b60fa63ab8964408d39e65d8b3b
liangcht <tj474474@gmail.com>
2016-11-06 21:06:43 -0500

    Declare wrr_info

--- kernel/kernel/sched/sched.h
+++ kernel/kernel/sched/sched.h
@@ -12,6 +12,13 @@
 
 extern __read_mostly int scheduler_running;
 
+#define MAX_CPUS 8
+struct wrr_info {
+	int num_cpus;
+	int nr_running[MAX_CPUS];
+	int total_weights[MAX_CPUS];
+}
+
 /*
  * Convert user-nice values [ -20 ... 0 ... 19 ]
  * to static priority [ MAX_RT_PRIO..MAX_PRIO-1 ],
