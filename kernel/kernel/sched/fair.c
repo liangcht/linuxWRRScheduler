62fc9f1921a333ebf994efa76b14e7506290afd1
sloanchoi1124 <xc2315@barnard.edu>
2016-11-08 15:47:31 -0500

    added debuging code

--- kernel/kernel/sched/fair.c
+++ kernel/kernel/sched/fair.c
@@ -4241,7 +4241,8 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 {
 	struct cfs_rq *cfs_rq;
 	struct sched_entity *se = &p->se;
-
+	/*SLOAN-DEV*/
+	printk("####################################CFS PROCESS %s\n", p->comm);
 	for_each_sched_entity(se) {
 		if (se->on_rq)
 			break;
