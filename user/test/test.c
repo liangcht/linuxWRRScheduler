829e0049e707d66d366a76ac981f6660de3d2dec
Jin Fang <jf2966@columbia.edu>
2016-11-10 17:51:33 -0500

    test.c checkpatch

--- user/test/test.c
+++ user/test/test.c
@@ -14,35 +14,33 @@ struct wrr_info {
 	int total_weight[MAX_CPUS];
 };
 
-void check_wrr_info() {
+void check_wrr_info(void)
+{
 	struct wrr_info my_wrr_info;
 	int i;
 	while (1) {
 		syscall(244, &my_wrr_info);
 		printf("Num of CPU : %d\n", my_wrr_info.num_cpus);
 		printf("Task running using wrr on cpus :");
-		for (i = 0; i < MAX_CPUS; ++i) {
+		for (i = 0; i < MAX_CPUS; ++i)
 			printf("%d\t", my_wrr_info.nr_running[i]);
-		}
 		printf("\n");
 		printf("Total weight using wrr on cpus :");
-		for (i = 0; i < MAX_CPUS; ++i) {
+		for (i = 0; i < MAX_CPUS; ++i)
 			printf("%d\t", my_wrr_info.total_weight[i]);
-		}
 		printf("\n");
-	
+
 		sleep(1);
 	}
 }
 
 int main(void)
 {
-	
 	int n = 5000;
 	pid_t pid[5000];
 	int i;
-	
-	//syscall(245, 100);
+
+	/*syscall(245, 100);*/
 	setuid(10001);
 	for (i = 0; i < n; i++) {
 		pid[i] = fork();
@@ -51,14 +49,14 @@ int main(void)
 			printf("%s\n", strerror(errno));
 			exit(EXIT_FAILURE);
 		} else if (pid[i] == 0) {
-			while(1)
-				usleep(100);	
+			while (1)
+				usleep(100);
 			exit(EXIT_SUCCESS);
 		}
-	}	
-	
+	}
+
 	check_wrr_info();
-	
+
 	int status;
 	pid_t w_pid;
 	while (1) {
